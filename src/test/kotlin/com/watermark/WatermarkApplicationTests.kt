12b67b4443e6d68b76492c19530d1671d21b53da
onenayooW <88319934+onenayooW@users.noreply.github.com>
2023-05-29 21:30:19 +0700

    feat: init project

---
+++ src/test/kotlin/com/watermark/WatermarkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.watermark
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WatermarkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
